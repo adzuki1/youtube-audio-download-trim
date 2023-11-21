05417b6469cf8750233d969c853a23cf11e3c2b0
adzuki1 <mneto.fis@gmail.com>
2023-11-21 16:41:55 -0300

    Defined driver function

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,13 @@
 import os
 import openpyxl
-import pydub
 import re
 from pytube import YouTube
 from pytube.exceptions import RegexMatchError
-from moviepy.editor import VideoFileClip
 from moviepy.editor import AudioFileClip
 from queue import Queue
 from threading import Thread
 
-
+# Globals:
 # consts
 A = 0
 B = 1
@@ -18,6 +16,8 @@ D = 3
 E = 4
 
 download_queue = Queue()
+#download_dir = "/home/usuario/Documentos/musicas-formatura"
+download_dir = "/home/adzk/Documents/formatura"
 
 def downloadAudio(yt_url, new_folder, timestamps):
     print(f"Downloading and trimming {yt_url} to {new_folder}")
@@ -29,13 +29,6 @@ def downloadAudio(yt_url, new_folder, timestamps):
 
         os.makedirs(new_folder_path, exist_ok=True)
         file_path = stream.download(output_path=new_folder_path, filename=url.title)
-
-        # convert audio extention to mp3
-        # video_duration = url.length
-        # output_path = os.path.join(new_folder_path, f"{url.title}.mp3")
-        # clip = VideoFileClip(file_path)
-        # audio_clip = clip.audio
-        # audio_clip.write_audiofile(output_path)
 
         if timestamps:
             trimmed_output_path = os.path.join(new_folder_path, f'{url.title}_trim.mp3')
@@ -51,7 +44,6 @@ def downloadAudio(yt_url, new_folder, timestamps):
     except RegexMatchError as error:
         print(f"Download error for {url}: {error}")
 
-
 def timestampToSeconds(timestamp):
     # convert timestamp in str to sec int
     match = re.match(r'(\d+):(\d+)', timestamp)
@@ -59,8 +51,8 @@ def timestampToSeconds(timestamp):
     if match:
         minutes, seconds = map(int, match.groups())
         return (minutes * 60 + seconds)
-    return 0
 
+    return 0
 
 def trimAudio(file_path, output_path, timestamps):
     # get audio file
@@ -87,7 +79,6 @@ def trimAudio(file_path, output_path, timestamps):
     # save trimmed file
     trimmed_audio.write_audiofile(output_path)
 
-
 def processQueue():
     counter = 0
 
@@ -107,27 +98,29 @@ def processQueue():
         # task done
         download_queue.task_done()
 
+def main():
 
-# open excel file
-#workbook = openpyxl.load_workbook("/home/usuario/Documentos/test.xlsx") # dir
-workbook = openpyxl.load_workbook("/home/adzk/youtube-audio-download-trim/test.xlsx")
-worksheet = workbook.active
-#download_dir = "/home/usuario/Documentos/musicas-formatura"
-download_dir = "/home/adzk/Documents/formatura"
+    # open excel file
+    #workbook = openpyxl.load_workbook("/home/usuario/Documentos/test.xlsx") # dir
+    workbook = openpyxl.load_workbook("/home/adzk/youtube-audio-download-trim/test.xlsx")
+    worksheet = workbook.active
 
-#queue
-worker_thread = Thread(target=processQueue)
-worker_thread.start()
+    #queue
+    worker_thread = Thread(target=processQueue)
+    worker_thread.start()
 
-# reading xlsx data
-for row in worksheet.iter_rows(min_row=2, values_only=True):
-    new_folder = row[C]
-    yt_url = row[D]
-    timestamps = row[E]
-    download_queue.put((yt_url, os.path.join(download_dir, new_folder), timestamps))
+    # reading xlsx data
+    for row in worksheet.iter_rows(min_row=2, values_only=True):
+        new_folder = row[C]
+        yt_url = row[D]
+        timestamps = row[E]
+        download_queue.put((yt_url, os.path.join(download_dir, new_folder), timestamps))
 
-# end of the queue
-download_queue.put(None)
+    # end of the queue
+    download_queue.put(None)
 
-# wait for the worker thread to finish
-worker_thread.join()
+    # wait for the worker thread to finish
+    worker_thread.join()
+
+if __name__ == "__main__":
+    main()
